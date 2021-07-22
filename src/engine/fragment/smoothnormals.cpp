4609533241a8042b16a0e1450a341305b22b9509
darias <dariasteam94@gmail.com>
2021-07-22 03:20:50 +0100

    Smooth normals shader

--- src/engine/fragment/smoothnormals.cpp
+++ src/engine/fragment/smoothnormals.cpp
@@ -2,7 +2,7 @@
 
 // x cut point between point-v1 line and v2-v3 line
 Vector2 SmoothNormals::cut_point(const Point2& p, const Point3& v1,
-                                 const Point3& v2, const Point3& v3) const {
+                  const Point3& v2, const Point3& v3) const {
 
   // Director vector for point to v1 line
   Vector2 dir_1 = Point2(v1.x(), v1.y()) - p;
@@ -32,16 +32,16 @@ Vector2 SmoothNormals::cut_point(const Point2& p, const Point3& v1,
  * 3. Assign a weight as normalice(vec1) / normalice(vec2) to each vertex
  *
  *
- * 4. Calculate the influence of each vertex normal with the weights and assign
+ * 4. Calculate the influece of each vertex normal with the weights and assign
  * to the current point
  */
 
 void SmoothNormals::operator ()(unsigned pixel_index) {
   auto& triangle = get_triangle_at_pixel_index(pixel_index);
-  P2i p1 = pixel_index_to_screen_coordinates(pixel_index);
+  Point2i p1 = pixel_index_to_screen_coordenates(pixel_index);
   Point2 p {double(p1.X), double(p1.Y)};
 
-  // Cut point between vertex-point line and vertex-vertex line (triangle edge)
+  // Cut point between vertex-point line and vertex-vertex line
   Vector2 ap_bc = cut_point(p, triangle.a, triangle.b, triangle.c);
   Vector2 bp_ac = cut_point(p, triangle.b, triangle.a, triangle.c);
   Vector2 cp_ab = cut_point(p, triangle.c, triangle.a, triangle.b);
@@ -62,16 +62,22 @@ void SmoothNormals::operator ()(unsigned pixel_index) {
   double C_d = 1.0 - Point2::vector_module(C) / max_distance_C;
 
   // Normal vectors of the vertices
-  const Normal2& n_a = triangle.normal_a;
-  const Normal2& n_b = triangle.normal_b;
-  const Normal2& n_c = triangle.normal_c;
+  Vector2 n_a = {triangle.normal_a.X, triangle.normal_a.Y};
+  Vector2 n_b = {triangle.normal_b.X, triangle.normal_b.Y};
+  Vector2 n_c = {triangle.normal_c.X, triangle.normal_c.Y};
 
-  auto final_vec = (n_a * A_d) + (n_b * B_d) + (n_c * C_d);
+  Vector2 n = {triangle.normal.x(), triangle.normal.y()};
 
-  unsigned char r = 128 + final_vec.X;
-  unsigned char g = 128 + final_vec.Y;
+  Vector2 final_vec = (n_a * A_d) + (n_b * B_d) + (n_c * C_d);
+
+
+  double x = final_vec.X;
+  double y = final_vec.Y;
+
+  unsigned char r = 128 + std::round(127.0 * x);
+  unsigned char g = 128 + std::round(127.0 * y);
 
   buffers.normal_buffer.set(pixel_index * 3 + 0, r);
   buffers.normal_buffer.set(pixel_index * 3 + 1, g);
-  //buffers.normal_buffer.set(pixel_index * 3 + 2, 255);
+  buffers.normal_buffer.set(pixel_index * 3 + 2, 255);
 }
